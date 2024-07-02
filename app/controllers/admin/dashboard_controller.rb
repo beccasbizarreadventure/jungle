ffc1631321c0fe06d5185708ea06f76daf16517a
Rebecca <rebecca90469@gmail.com>
2024-07-02 00:08:16 -0400

    Add admin inventory page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["admin_username"], password: ENV["admin_password"]
 
   def show
+    @category_count = Category.count
+    @product_count = Product.count
   end
 end
